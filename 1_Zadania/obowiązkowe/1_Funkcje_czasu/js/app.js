a053c560dcb7abcd38a2f4e5977b0a58f0ba2151
Szlachta Patryk <patryk.szlachta@microsoft.wsei.edu.pl>
2019-10-06 09:25:40 +0200

    array 3

--- 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -1,3 +1,4 @@
+/*
 function distFromAverage(array){
     let sum = 0;
 
@@ -8,6 +9,21 @@ function distFromAverage(array){
     let average = sum / array.length;
 
     return array.map(element => {
-        return element - average;
+        return Math.abs(element - average);
     })
+}
+*/
+
+
+function fruits(){
+const fruit=['truskawka', 'jablko', 'gruszka', 'malina'];
+console.log(fruit)
+}
+fruits()
+
+function multiply(array){
+    var multiply = 1;
+    for(var i=0; array.length; i++){
+        multiply =multiply*array[i];
+    }
 }
