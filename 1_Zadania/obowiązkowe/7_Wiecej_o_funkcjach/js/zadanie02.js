a72ac63efc711bb57420e7df2dfdf26ae5cc45d7
Szlachta Patryk <patryk.szlachta@microsoft.wsei.edu.pl>
2019-12-08 09:27:17 +0100

    task 8

--- 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie02.js
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie02.js
@@ -3,12 +3,14 @@ function callOtherFunction(nameOfFunction) {
 
     var randomNumber1 = Math.random() * 20;
     var randomNumber2 = Math.random() * 10;
-    nameOfFunction(randomNumber1, randomNumber2);
+    var randomNumber3 = Math.random() * 30;
+    nameOfFunction(randomNumber1, randomNumber2, randomNumber3);
 }
 
-callOtherFunction(function (a, b) {
+callOtherFunction(function (a, b, c) {
     console.log("Pierwsza liczba:", a);
     console.log("Druga liczba", b)
-    console.log("Wynik", a + b)
+    console.log("Trzecia liczba", c)
+    console.log("Wynik", a + b + c)
 })
 
---
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie03.js
@@ -0,0 +1,18 @@
+hello = () => {
+    console.log('czesc')
+}
+
+hello();
+
+
+
+hello2 = () => {
+    console.log('witaj11')
+}
+hello2();
+
+
+hoisted();
+function hoisted() {
+    console.log('witaj ostatnie')
+}
